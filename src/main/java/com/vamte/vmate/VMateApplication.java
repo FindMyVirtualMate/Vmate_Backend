300b524b39c1f31516fa66098a9651c609b017ca
조유민 <nationmilo@gmail.com>
2024-06-11 12:15:11 +0900

    feat: origin init files

---
+++ src/main/java/com/vamte/vmate/VMateApplication.java
@@ -0,0 +1,13 @@
+package com.vamte.vmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vamte/vmate/VMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamte.vmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
