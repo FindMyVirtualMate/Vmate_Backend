6ff033b1a92673f4508c5f3a898f23df40b717b7
조유민 <nationmilo@gmail.com>
2024-09-19 14:21:10 +0900

    feat: settings initAll Data

---
+++ src/main/java/com/vamte/global/initData/All.java
@@ -0,0 +1,23 @@
+﻿package com.vamte.global.initData;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@Slf4j
+@RequiredArgsConstructor
+public class All {
+    
+    
+    @Bean
+    @Order(2)
+    public ApplicationRunner initAll() {
+        return args -> {
+            log.info("initAll");
+        };
+    }
+}
